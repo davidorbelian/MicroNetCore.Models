ac755555371a9db51fe8e0f8ddc25194763b1cc8
David Orbelian <david.orbelian@gmail.com>
2017-09-20 16:52:46 +0400

    Add ModelList. Update package version.

---
+++ src/MicroNetCore.Models/Collections/ModelList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MicroNetCore.Models.Collections
+{
+    public sealed class ModelList : List<IModel>, IModelList
+    {
+        public ModelList(IEnumerable<IModel> models)
+            : base(models)
+        {
+        }
+    }
+}
